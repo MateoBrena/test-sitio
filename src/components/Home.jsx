025ef51d2bc197974bd3f927e925cfcb447639f4
MateoBrena <matubrena@gmail.com>
2023-12-29 12:09:51 -0300

    Inicio mobile modificado

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import CarouselFade from "./Carousel";
+import "../css/home.css";
 
 export default function Home() {
     return (
         <>
         <Header></Header>
         <CarouselFade></CarouselFade>
+        <section className="textos-inicio">
+            <div>
+                <h2>Misión</h2>
+                <p> Que cada familia pueda cumplir el sueño de tener su casa propia.
+                    Nos inspira ese momento de entregar la llave en mano a su dueño.
+                </p>
+            </div>
+            <div>
+                <h2>Materialización</h2>
+                <p> Ajustaremos el diseño a tu presupuesto y estilo de vida. 
+                    Cuando quedes completamente satisfecho, haremos que tome forma.
+                </p>
+            </div>
+        </section>
         <Footer></Footer>
         </>
     )
