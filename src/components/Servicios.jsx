788b51d22c8ff5543fb25157bffe94bc8d3b958f
MateoBrena <matubrena@gmail.com>
2024-02-28 19:27:47 -0300

    Apartado servicios terminado

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,4 +1,5 @@
-import archivo from "../db/servicios.pdf"
+import archivo1 from "../db/Procesos.pdf"
+import archivo2 from "../db/Calidades.pdf"
 import "../css/servicios.css"
 
 export default function Servicios() {
@@ -7,23 +8,26 @@ export default function Servicios() {
             <img src="/images/banner-servicios.jpg" alt="foto-servicios" className="d-block w-100" />
             <div className="contenedor-servicios">
                 <div className="primer-servicio">
-                    <img src="https://media.istockphoto.com/id/929029342/es/foto/imagen-del-ingeniero-equipo-comprueba-planos-de-construcci%C3%B3n-en-el-nuevo-proyecto-con.jpg?s=612x612&w=0&k=20&c=lHzXypeAszefJkgj3_vKhXrBRyfsLUb2kpfb9gKjy7o=" alt="foto-servicio-uno" className="imagen-servicios" />
+                    <img src="/images/san-ramon-2.jpg" alt="foto-servicio-uno" className="imagen-servicios" />
                     <div>
-                        <h3> Servicio Uno </h3>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Dicta repudiandae magni tenetur exercitationem impedit ex earum dolor, fugiat vel quae,
-                        sunt illum amet ullam. Minima alias illum mollitia praesentium nemo!
-                        <p><button className="btn btn-dark descarga"><a href={archivo} download="Servicios"> Descargar </a></button></p>
+                        <h3 className="titulo-servicio"> Procesos </h3>
+                        <p className="textos-servicios">
+                            En el siguiente archivpo PDF podrás ver todo sobre nuestros procesos, metodología de trabajo, presupuestos,
+                            etapas de obra y valor de metro cuadrado. Además de todo lo lo que incluye nuestro presupuesto all inclusive.
+                            Para descargarlo, haz click en el botón a continuación.
+                        </p>
+                        <p><button className="btn btn-dark descarga"><a href={archivo1} download="Procesos"> Descargar </a></button></p>
                     </div>
                 </div>
                 <div className="segundo-servicio">
-                    <img src="https://media.istockphoto.com/id/507032313/es/foto/vista-del-edificio-en-construcci%C3%B3n.jpg?s=612x612&w=0&k=20&c=ztUN2DXz94JAe3MAeJMT7nyoTtR4WhBLBnf8BIRCbyk=" alt="foto-servicio-dos" className="imagen-servicios" />
+                    <img src="/images/belgrano-4.jpg" alt="foto-servicio-dos" className="imagen-servicios" />
                     <div>
-                        <h3> Servicio Dos </h3>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        Dicta repudiandae magni tenetur exercitationem impedit ex earum dolor, fugiat vel quae,
-                        sunt illum amet ullam. Minima alias illum mollitia praesentium nemo!
-                        <p><button className="btn btn-dark descarga"><a href={archivo} download="Servicios"> Descargar </a></button></p>
+                         <h3 className="titulo-servicio"> Calidades </h3>
+                         <p className="textos-servicios">
+                            En el siguiente archivpo PDF podrás encontrar información sobre los materiales y calidades utilizados
+                            en nuestros trabajos. Para descargarlo, haz click en el botón a continuación.
+                        </p>
+                        <p><button className="btn btn-dark descarga"><a href={archivo2} download="Calidades"> Descargar </a></button></p>
                     </div>
                 </div>
             </div>
